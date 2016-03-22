2752f1b5bc17f3dd10d095d2bb86eb11ea462b35
Mic <mic.cassano@gmail.com>
2016-03-22 18:39:13 +0000

    Remove unused boilerplate

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,17 +1,12 @@
-'use strict';
+(function() {
+  'use strict';
 
-/**
- * @ngdoc function
- * @name clientApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the clientApp
- */
-angular.module('clientApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  angular.module('clientApp')
+    .controller('MainCtrl', function () {
+      this.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+      ];
+    });
+}());
--- server/app.js
+++ server/app.js
@@ -5,21 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+/**
+* Route Imports
+*/
 
 var app = express();
 
-
-
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// error handlers
 
 /**
  * Development Settings
@@ -59,5 +55,10 @@ if (app.get('env') === 'production') {
         });
     });
 }
+
+/**
+* Routes
+*/
+
 
 module.exports = app;
