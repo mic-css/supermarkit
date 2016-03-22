936316385aae81f6b2c861961efd04eb039852b0
Mic <mic.cassano@gmail.com>
2016-03-22 16:11:44 +0000

    Generate default sign up controller using Yeoman

---
+++ client/app/scripts/controllers/signup.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:SignupCtrl
+ * @description
+ * # SignupCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('SignupCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
