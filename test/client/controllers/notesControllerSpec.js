f53750bfec76c45cd087f8876da075c5fa676600
Mic <mic.cassano@gmail.com>
2016-03-26 19:28:57 +0000

    Failing angular notesController spec

---
+++ test/client/controllers/notesControllerSpec.js
@@ -0,0 +1,53 @@
+describe('Controller: NotesCtrl', function () {
+  'use strict';
+
+  var NotesCtrl, mockNoteFactory;
+  var mockNotes = [
+    {
+      "_id": "56f6d8cbd7c4711bae9abd52",
+      "title": "Note title",
+      "content": "Example note content",
+      "__v": 0,
+      "created_at": "2016-03-26T18:45:31.155Z"
+    },
+    {
+      "_id": "56f6c3153f96f241a0412af2",
+      "title": "To-do",
+      "content": "Feed the dinosaur",
+      "__v": 0,
+      "created_at": "2016-03-26T17:12:53.848Z"
+    },
+    {
+      "_id": "56f6c32e3f96f241a0412af3",
+      "title": "Kill Evernote",
+      "content": "Donwload markpad instead :)",
+      "__v": 0,
+      "created_at": "2016-03-26T17:13:18.840Z"
+    }
+  ];
+
+  beforeEach(module('markpad', function ($provide) {
+    mockNoteFactory = {
+      query: function () {
+        return mockNotes;
+      }
+    };
+
+    spyOn(mockNoteFactory, 'query').and.callThrough();
+
+    $provide.factory('Note', function () {
+      return mockNoteFactory;
+    });
+  }));
+
+  beforeEach(inject(function ($controller, $rootScope) {
+    NotesCtrl = $controller('NotesCtrl', {
+      $scope: $rootScope,
+      Note: mockNoteFactory
+    });
+  }));
+
+  it('should query the Note factory to load all notes', function () {
+    expect(mockNoteFactory.query).toHaveBeenCalled();
+  });
+});
