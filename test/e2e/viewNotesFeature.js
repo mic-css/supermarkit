e49b8bec8ab2d970a91c4447be9ab4efb3185960
Mic <mic.cassano@gmail.com>
2016-03-26 12:13:00 +0000

    Add note preview

--- test/e2e/viewNotesFeature.js
+++ test/e2e/viewNotesFeature.js
@@ -7,8 +7,11 @@ describe('View notes', function () {
 
   it("displays all existing notes", function () {
     var notes = element.all(by.css('.note'));
-    var firstNote = notes.first();
+    var firstNoteTitle = element.all(by.css('.note .title')).first().getText();
+    var firstNotePreview = element.all(by.css('.note .preview')).first().getText();
+
     expect(notes.count()).toBe(3);
-    expect(firstNote.getText()).toEqual('Test Note 1');
+    expect(firstNoteTitle).toEqual('Test Note 1');
+    expect(firstNotePreview).toEqual('This is a test');
   });
 });
