91c86473ce93abef8785e4f62236871c3645395c
itsindigo <matthewbridgesbusiness@gmail.com>
2016-03-28 13:35:12 +0100

    Initial User Creation Scaffold Working through mherman tutorial listed in the Wiki, initial dependencies and model installed.

--- app.js
+++ app.js
@@ -6,31 +6,16 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var config = require('./_config');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var indexRouter = require('./app/routes/indexRouter');
+var usersRouter = require('./app/routes/usersRouter')
 var notesController = require('./app/controllers/notesController');
+
 
 var app = express();
 var db = process.env.MONGOLAB_URI || config.mongoURI[app.settings.env];
-
-// *** mongoose *** ///
-
-mongoose.connect(db, function(err, res) {
-  if(err) {
-    console.log('Error connecting to the database. ' + err);
-  } else {
-    console.log('Connected to Database: ' + db);
-  }
-});
-
-// *** parsing *** ///
-
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/public')));
 
 // *** views *** ///
 
@@ -41,6 +26,37 @@ app.set('view engine', 'jade');
 
 app.use('/', indexRouter);
 app.use('/notes', notesController);
+
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// passport config
+var Account = require('./app/models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// *** mongoose *** ///
+
+mongoose.connect(db, function(err, res) {
+  if(err) {
+    console.log('Error connecting to the database. ' + err);
+  } else {
+    console.log('Connected to Database: ' + db);
+  }
+});
 
 // *** error handling *** ///
 
