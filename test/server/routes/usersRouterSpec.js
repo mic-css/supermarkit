8b33d97f4576a9a9f92d3c1d7b2ad1b6275d164f
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-03-30 16:22:49 +0100

    Spec tests added to usersRouterSpec to check for router level verifictation of username and password validity.

--- test/server/routes/usersRouterSpec.js
+++ test/server/routes/usersRouterSpec.js
@@ -31,9 +31,9 @@ describe('User', function() {
       .send(user)
       .end(function (err, res) {
         res.should.be.json;
-        res.should.have.status(200);
         res.body.should.have.property('info');
         res.body.info.should.equal('success');
+        res.should.have.status(200);
         done();
       });
   });
@@ -45,21 +45,52 @@ describe('User', function() {
       .send(user)
       .end(function (err, res) {
         res.should.be.json;
-        res.should.have.status(200);
         res.body.should.have.property('info');
         res.body.info.should.equal('Login Successful');
+        res.should.have.status(200);
         done();
       });
   });
 
-  it('should not a allow a register to signup on a used email', function (done) {
+  it('should not allow a user to register an email already in the database.', function (done) {
     chai.request(app)
       .post('/users/register')
       .send(user)
       .end(function (err, res) {
         res.should.be.json;
-        res.should.have.status(500);
+        res.should.have.status(400);
+        res.body.should.have.property('error');
+        res.body.error.message.should.include('User already exists with username Username');
         done();
       });
   });
+
+  it('should not allow a user to register without a username', function(done){
+    user = {username: "", email: "test@test.com", password: "Password"};
+    chai.request(app)
+      .post('/users/register')
+      .send(user)
+      .end(function (err, res) {
+        res.should.be.json;
+        res.body.should.have.property('error');
+        res.body.error.message.should.include('Field username is not set');
+        res.should.have.status(400);
+        done();
+      });
+  });
+
+  it('should not allow a user to register without a password', function(done){
+    user = {username: "Username2", email: "test@test.com", password: ""};
+    chai.request(app)
+      .post('/users/register')
+      .send(user)
+      .end(function (err, res) {
+        res.should.be.json;
+        res.body.should.have.property('error');
+        res.body.error.message.should.include('Password argument not set');
+        res.should.have.status(400);
+        done();
+      });
+  });
+
 });
