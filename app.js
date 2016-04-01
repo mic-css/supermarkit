5e4d3d89641250f2e3204c9888f70e74a5cfd2d6
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-04-01 13:03:55 +0100

    Accidentally commented out express server line in app.js.  Uncommented now.

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(require('express-session')({
 }));
 // app.use(passport.initialize());
 // app.use(passport.session());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // *** routing *** ///
 
